0ef59982594ae69e250df883480c2f86a093abea
Fabio Kusaba <fabinhokb44@gmail.com>
2025-03-13 21:49:36 -0300

    inicialização e configurações iniciais

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import unusedImports from "eslint-plugin-unused-imports";
+import airbnbBase from "eslint-config-airbnb-base";
+import airbnbBaseTypescript from "eslint-config-airbnb-base-typescript";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      "unused-imports": unusedImports
+    },
+    rules: {
+      ...airbnbBase.rules,
+      ...airbnbBaseTypescript.rules,
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        }
+      ]
+    },
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"]
+        }
+      }
+    }
+  },
+];

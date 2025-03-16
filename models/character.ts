573e71bf9dd914371e7b2db5070778f7f85cc5b8
Fabio Kusaba <fabinhokb44@gmail.com>
2025-03-16 14:12:40 -0300

    adicionado jsdocs nos métodos da classe

--- models/character.ts
+++ models/character.ts
@@ -11,6 +11,10 @@ export type Character = InferInput<typeof CharacterSchema> & { id: number };
 
 const characters: Map<number, Character> = new Map();
 
+/**
+ * Retrieves all characters
+ * @returns {Character[]} - An array of all characters
+ */
 export const getAllCharacters = (): Character[] => {
     // Transformando um Map em um Array
     // characters.values() vai nos retornar um iterador e esse iterador é o que vamos utilizar para justamente transformar
@@ -18,10 +22,20 @@ export const getAllCharacters = (): Character[] => {
     return Array.from(characters.values());
 }
 
+/**
+ * Retrieves a character by its ID
+ * @param {number} id - The ID of the user
+ * @returns {Character | undefined} - The character with the given ID, or undefined if not found
+ */
 export const getCharacterById = (id: number): Character | undefined => {
     return characters.get(id);
 }
 
+/**
+ * Adds a new character
+ * @param {Character} character - The character to add 
+ * @returns {Character} - The newly added character with generated ID
+ */
 export const addCharacter = (character: Character): Character => {
     // Criando um novo character
     const newCharacter = {
@@ -34,6 +48,12 @@ export const addCharacter = (character: Character): Character => {
     return newCharacter;
 }
 
+/**
+ * Updates an existing character
+ * @param {number} id - The ID of the character to update
+ * @param {Character} updatedCharacter - The updated character data 
+ * @returns {Character | null} - The updated character, or null if the character was not found
+ */
 export const updateCharacter = (id: number, updatedCharacter: Character): Character | null => {
     // Verificando se temos o character
     if (!characters.has(id)) {
@@ -48,6 +68,11 @@ export const updateCharacter = (id: number, updatedCharacter: Character): Charac
     return updatedCharacter;
 }
 
+/**
+ * Deletes a character by its ID
+ * @param {number} id - The ID of the character to delete
+ * @returns {boolean} - True if character was deleted, false if the character was not found
+ */
 export const deleteCharacter = (id: number): boolean => {
     // Verificando se temos o character
     if (!characters.has(id)) {

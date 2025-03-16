a09c9f6533cafb3975ca71283f8cd8645831c34f
Fabio Kusaba <fabinhokb44@gmail.com>
2025-03-16 19:24:29 -0300

    adicionando a porta da aplicação na config

--- config.ts
+++ config.ts
@@ -2,7 +2,8 @@
 export const config = {
     // Utilizamos process.env por questões de privacidade, segurança da aplicação
     // Tudo o que for ligado a environments vão nesse arquivo .env
-    jwtSecret: process.env.JWT_SECRET as string || "My_Secret_Key"
+    jwtSecret: process.env.JWT_SECRET as string || "My_Secret_Key",
+    port: process.env.PORT as string || 4000
 }
 
 export default config;

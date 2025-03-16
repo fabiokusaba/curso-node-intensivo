5310428f01eb435396fb02fdf443d9c10dcd5e9e
Fabio Kusaba <fabinhokb44@gmail.com>
2025-03-16 13:20:07 -0300

    criação do arquivo index para exportação dos arquivos da pasta models

---
+++ models/index.ts
@@ -0,0 +1,5 @@
+// Se dentro de um pasta se exporta informação de mais de um arquivo utiliza-se o index
+export * from "./character";
+export * from "./api";
+export * from "./revokeToken";
+export * from "./users";
